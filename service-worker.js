74e3b372c1e0e594878ea94d7a15496c83c8b91f
AyandaMabasa <preciousmabasa684@gmail.com>
2025-06-09 19:28:57 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,25 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('todo-cache').then((cache) => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/styles.css',
-        '/app.js',
-        '/manifest.json',
-        '/icons/android-chrome-192x192.png',
-        '/icons/android-chrome-512x512.png'
-      ]);
-    })
+const CACHE_NAME = 'todo-cache-v1';
+const urlsToCache = [
+  '/',
+  '/todo-list-app/',
+  '/todo-list-app/index.html',
+  '/todo-list-app/styles.css',
+  '/todo-list-app/app.js',
+  '/todo-list-app/icons/icon-192x192.png',
+  '/todo-list-app/icons/icon-512x512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
+
 
